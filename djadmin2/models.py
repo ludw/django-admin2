edd88fc348c8f038d9661facd4d220de944a4cec
Gregor Müllegger <gregor@muellegger.de>
2013-05-18 17:50:09 +0200

    Adding first prototype of a List/Create API view.

--- djadmin2/models.py
+++ djadmin2/models.py
@@ -5,6 +5,11 @@ synonymous with the django.contrib.admin.sites model.
 
 """
 
+<<<<<<< Updated upstream
+=======
+from djadmin2 import apiviews
+from djadmin2 import views
+>>>>>>> Stashed changes
 
 try:
     import floppyforms as forms
@@ -73,3 +78,66 @@ class Admin2(BaseAdmin2):
     search_fields = ()
     save_as = False
     save_on_top = False
+<<<<<<< Updated upstream
+=======
+
+    #  Views
+    index_view = views.ModelListView
+    create_view = views.ModelAddFormView
+    update_view = views.ModelEditFormView
+    detail_view = views.ModelDetailView
+    delete_view = views.ModelDeleteView
+
+    api_index_view = apiviews.ModelListCreateAPIView
+
+    def __init__(self, model, **kwargs):
+        self.model = model
+
+    def get_urls(self):
+        return patterns('',
+            url(
+                regex=r'^$',
+                view=self.index_view.as_view(model=self.model),
+                name='index'
+            ),
+            url(
+                regex=r'^create/$', 
+                view=self.create_view.as_view(model=self.model), 
+                name='create'
+            ),
+            url(
+                regex=r'^(?P<pk>[0-9]+)/$',
+                view=self.detail_view.as_view(model=self.model), 
+                name='detail'
+            ),
+            url(
+                regex=r'^(?P<pk>[0-9]+)/update/$',
+                view=self.update_view.as_view(model=self.model),
+                name='update'
+            ),
+            url(
+                regex=r'^(?P<pk>[0-9]+)/delete/$',
+                view=self.delete_view.as_view(model=self.model),
+                name='delete'
+            ),
+        )
+
+    def get_api_urls(self):
+        return patterns('',
+            url(
+                regex=r'^$',
+                view=self.api_index_view.as_view(model=self.model),
+                name='api-index'
+            ),
+        )
+
+    @property
+    def urls(self):
+        # We set the application and instance namespace here
+        return self.get_urls(), None, None
+
+    @property
+    def api_urls(self):
+        # We set the application and instance namespace here
+        return self.get_api_urls(), None, None
+>>>>>>> Stashed changes
