d0a648db96f107c701f73d8ad2efb888d9d10313
Gregor Müllegger <gregor@muellegger.de>
2013-05-19 10:09:04 +0200

    Adding rest_framework to the INSTALLED_APPS so that the explorable API browser works.

--- example/example/settings.py
+++ example/example/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'rest_framework',
     'djadmin2',
     'blog',
 )
